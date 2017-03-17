150651fa2fe70ee6636209f81d64dd1efc2d8d2b
Cahlen Humphreys <cahlen@gmail.com>
2017-03-17 13:54:56 -0600

    Reformats yelp data to valid json file

---
+++ sample_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# 
+# Reformat Yelp data to valid json file
+
+import sys
+import json
+import codecs
+
+DATA_FILE = "data/yelp_academic_dataset_business.json"
+
+def open_data_file(filename):
+    return open(filename, "r")
+
+def main():
+    # Open data file and temporary file
+    f = open_data_file(DATA_FILE)
+    data = []
+    with codecs.open("data/valid_json.json", "rU", "utf-8") as valid:
+        for line in f:
+            data.append(json.loads(line))
+    f.close()
+
+    with open("data/valid_json.json", "w") as valid:
+        json.dump(data, valid)
+
+if __name__ == "__main__":
+    main()
