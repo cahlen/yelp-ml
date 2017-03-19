434085226e7beba6d82e39c7108e28f8a78f13c2
Cahlen Humphreys <cahlen@gmail.com>
2017-03-19 03:42:06 -0600

    Added feature selection

--- ml-yelp.py
+++ ml-yelp.py
@@ -4,10 +4,13 @@ import pandas as pd
 import os, sys
 import numpy as np
 from sklearn import preprocessing, svm, cross_validation, tree, metrics
-#from sklearn.ensemble import ske
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2 
+from sklearn.preprocessing import MinMaxScaler
 
 np.random.seed(0)
 
@@ -41,13 +44,35 @@ def main():
     X = df.drop(['is_open'], axis=1).values
     y = df['is_open'].values
 
+    # Scale data
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
+    y_scaled = scaler.fit_transform(y)
+
     # Cross validate
-    X_train, X_test, y_train, y_test =cross_validation.train_test_split(X,y,test_size=0.3)
+    X_train, X_test, y_train, y_test =cross_validation.train_test_split(X_scaled,y_scaled,test_size=0.3)
+
+    # Feature Selection
+    """
+    print(X_train.shape)
+    chi2k = SelectKBest(chi2, k=2) #.fit_transform(X_train, y_train)
+    X_new_train = chi2k.fit_transform(X_train, y_train)
+    print(X_new_train.shape)
+    mask = chi2k.get_support()
+    new_features = []
+    for bool, feature in zip(mask, names):
+        if bool:
+            new_features.append(feature)
+    print(new_features)
+    sys.exit(0)
+    """ 
 
     # Decision Tree
     clf_dt = tree.DecisionTreeClassifier(max_depth=10)
     clf_dt.fit(X_train, y_train)
+    scores = cross_val_score(clf_dt, X_train, y_train)
     print("Decision Tree: " + str(clf_dt.score(X_test,y_test)))
+    print("Cross Validation Mean: " + str(scores.mean()))
 
     # Random Forest
     clf_rf = RandomForestClassifier(n_estimators=50)
